121dc6e47a71a733ddd2948c7048be99c66565af
nikolozi0 <nikachedlishvili25@gmail.com>
2023-10-17 19:21:17 +0400

    update

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,18 @@
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-  );
 
-  workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.NetworkFirst()
-  );
+// service-worker.js
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open("static".then(cache => {
+      return cache.addAll(["./", "./icon.png", "./style.css", "./script.js", "./index.html"])
 
-  
+      })
+      ));
+  }
+);
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
